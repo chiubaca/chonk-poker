cc2da32e1690e5db8bfff87a8182fed66e569314
Alex Chiu <alexchiu11@gmail.com>
2025-12-17 22:18:56 +0000

    user server fn

---
+++ src/lib/user.ts
@@ -0,0 +1,29 @@
+import { createServerFn } from "@tanstack/react-start";
+import { getCookies, setCookie } from "@tanstack/react-start/server";
+import z from "zod";
+
+export const USER_COOKIE_KEY_ENUM = {
+	USER_ID: "user_id",
+	USERNAME: "user_name",
+} as const;
+
+const UserSchema = z.object({
+	userId: z.string(),
+	userName: z.string(),
+});
+
+export const setUserServerFn = createServerFn()
+	.inputValidator(UserSchema)
+	.handler(({ data }) => {
+		setCookie(USER_COOKIE_KEY_ENUM.USER_ID, data.userId);
+		setCookie(USER_COOKIE_KEY_ENUM.USERNAME, data.userName);
+	});
+
+export const getUserServerFn = createServerFn().handler(() => {
+	const cookies = getCookies();
+
+	return {
+		userName: cookies[USER_COOKIE_KEY_ENUM.USERNAME],
+		userId: cookies[USER_COOKIE_KEY_ENUM.USER_ID],
+	};
+});
