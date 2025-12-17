97da2c22b9d4ece2cfc56d6852989887348adb77
Alex Chiu <alexchiu11@gmail.com>
2025-12-17 22:39:03 +0000

    setup do

---
+++ src/durable-objects/poker-room-do.ts
@@ -0,0 +1,114 @@
+import { DurableObject } from "cloudflare:workers";
+import { createActor } from "xstate";
+import {
+	type PlanningPokerMachineSnapshot,
+	planningPokerMachine,
+} from "@/state-machine/planning-poker-machine";
+import type {
+	PokerGameEvents,
+	PokerPlayer,
+} from "@/state-machine/planning-poker-machine.types";
+
+export class PokerRoomObject extends DurableObject<Env> {
+	sql: SqlStorage;
+
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+		this.sql = ctx.storage.sql;
+
+		this.sql.exec(`
+			CREATE TABLE IF NOT EXISTS poker_room(
+     		id         INTEGER PRIMARY KEY,
+				game_state TEXT
+      );
+		`);
+	}
+
+	async hey(test: string) {
+		console.log(test);
+
+		return "hey";
+	}
+
+	async gameAction(event: PokerGameEvents) {
+		const query = this.sql.exec(
+			`SELECT game_state FROM poker_room WHERE id = 1`,
+		);
+
+		const record = query.one();
+		if (!record) {
+			throw new Error("No game state found");
+		}
+
+		const gameState = JSON.parse(
+			record.game_state as string,
+		) as PlanningPokerMachineSnapshot;
+
+		const gameStateMachineActor = createActor(planningPokerMachine, {
+			snapshot: gameState,
+		}).start();
+
+		gameStateMachineActor.send(event);
+
+		const gameStateSnapshot =
+			gameStateMachineActor.getPersistedSnapshot() as PlanningPokerMachineSnapshot;
+
+		const serialisedGameState = JSON.stringify(gameStateSnapshot);
+		this.sql.exec(
+			`UPDATE poker_room SET game_state = ? WHERE id = 1`,
+			serialisedGameState,
+		);
+
+		const sockets = this.ctx.getWebSockets();
+		for (const socket of sockets) {
+			socket.send(serialisedGameState);
+		}
+	}
+
+	async getGameState() {
+		const query = this.sql.exec(
+			`SELECT game_state FROM poker_room WHERE id = 1`,
+		);
+
+		const record = query.one();
+		if (!record) {
+			return null;
+		}
+
+		return record.game_state as string;
+	}
+
+	async createRoom(player: Pick<PokerPlayer, "id" | "name">) {
+		const machine = createActor(planningPokerMachine).start();
+		machine.send({
+			type: "player.join",
+			player: { ...player, state: "choosing" },
+		});
+		const gameState = machine.getPersistedSnapshot();
+
+		this.sql.exec(
+			`INSERT OR REPLACE INTO poker_room (id, game_state) VALUES (?, ?)`,
+			1,
+			JSON.stringify(gameState),
+		);
+	}
+
+	async fetch(_: Request) {
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+		this.ctx.acceptWebSocket(server);
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
+	}
+
+	webSocketClose(
+		ws: WebSocket,
+		code: number,
+		reason: string,
+		wasClean: boolean,
+	): void | Promise<void> {
+		console.log("client closed");
+	}
+}
--- worker-entry.ts
+++ worker-entry.ts
@@ -1,5 +1,7 @@
 import handler, { createServerEntry } from '@tanstack/react-start/server-entry'
 
+export { PokerRoomObject } from './src/durable-objects/poker-room-do'
+
 export default createServerEntry({
   fetch(request) {
     return handler.fetch(request)
