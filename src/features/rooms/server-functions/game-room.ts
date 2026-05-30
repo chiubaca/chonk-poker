a5afe49579ca8086602b3a265e1e2349aee2f8d9
alex <alexchiu11@gmail.com>
2026-05-30 21:16:28 +0100

    auto format on save with oxc

--- src/features/rooms/server-functions/game-room.ts
+++ src/features/rooms/server-functions/game-room.ts
@@ -7,13 +7,13 @@ import { env } from "cloudflare:workers";
 import { pokerEventsSchema } from "@/features/poker/state-machine/planning-poker-machine.schemas";
 
 export const handleGameActionServerFn = createServerFn({ method: "POST" })
-	.inputValidator(
-		z.object({
-			pokerEvent: pokerEventsSchema,
-			roomId: z.string(),
-		}),
-	)
-	.handler(async ({ data }) => {
-		const stub = env.POKER_ROOM_DURABLE_OBJECT.getByName(data.roomId);
-		await stub.gameAction(data.pokerEvent);
-	});
+  .inputValidator(
+    z.object({
+      pokerEvent: pokerEventsSchema,
+      roomId: z.string(),
+    }),
+  )
+  .handler(async ({ data }) => {
+    const stub = env.POKER_ROOM_DURABLE_OBJECT.getByName(data.roomId);
+    await stub.gameAction(data.pokerEvent);
+  });
